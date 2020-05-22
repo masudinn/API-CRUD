b966500278d3f3b088b61f2ef111dd90dbd412df
masudinn <fachrudin.2000@students.amikom.ac.id>
2020-05-23 04:43:33 +0700

    menambahkan update.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,8 @@
 <?php
-$host="localhost";
+$server="localhost";
 $user="root";
 $pass="";
 $db="db_laundry";
 
-$konek = mysqli_connect($host,$user,$pass,$db) or die("Database MYSQL tidak terhubung");
+$konek = mysqli_connect($server,$user,$pass,$db) or die("Database MYSQL tidak terhubung");
+?>
